85f6fac0782af1af00999016dc2f96b49bdea1bf
Suyeol Yun <syyun@mit.edu>
2021-11-11 22:38:07 -0500

    add sample code for prime

--- file-changes/print.R
+++ file-changes/print.R
@@ -1,1 +1,16 @@
 print("R is fun")
+
+# is.prime <- function(num) {
+#    if (num == 2) {
+#       return(TRUE)
+#    } else if (any(num %% 2:(num-1) == 0)) {
+#       return(FALSE)
+#    } else {
+#       return(TRUE)
+#    }
+# }
+
+# for (i in 1:1000){
+#   bool <- is.prime(i)
+#   print(bool)
+# }
