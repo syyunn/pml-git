b9ab00e8ade9b6cc8161a52bf7c0fc3322815f09
Suyeol Yun <syyun@mit.edu>
2021-11-12 11:04:40 -0500

    add all

--- file-changes/print.R
+++ file-changes/print.R
@@ -1,14 +1,14 @@
 print("R is fun")
 
-# is.prime <- function(num) {
-#    if (num == 2) {
-#       return(TRUE)
-#    } else if (any(num %% 2:(num-1) == 0)) {
-#       return(FALSE)
-#    } else {
-#       return(TRUE)
-#    }
-# }
+is.prime <- function(num) {
+   if (num == 2) {
+      return(TRUE)
+   } else if (any(num %% 2:(num-1) == 0)) {
+      return(FALSE)
+   } else {
+      return(TRUE)
+   }
+}
 
 # for (i in 1:1000){
 #   bool <- is.prime(i)
